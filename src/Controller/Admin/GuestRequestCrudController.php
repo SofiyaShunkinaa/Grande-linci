4d01730f1b04afa5a9069ed5947f9264fe01a9c1
Sofiya <sofik_shu2@mail.ru>
2024-07-21 17:17:40 +0300

    create crud controllers

---
+++ src/Controller/Admin/GuestRequestCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\GuestRequest;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class GuestRequestCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return GuestRequest::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
