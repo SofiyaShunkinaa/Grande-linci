b38147a943751598a9cdcf4d75c0d01e46097b9e
Sofiya <sofik_shu2@mail.ru>
2024-07-22 23:43:52 +0300

    add fields to crud controller

--- src/Controller/Admin/ColorCrudController.php
+++ src/Controller/Admin/ColorCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
 
 class ColorCrudController extends AbstractCrudController
 {
@@ -15,14 +17,13 @@ class ColorCrudController extends AbstractCrudController
         return Color::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('breed'),
+            TextField::new('name'),
         ];
     }
-    */
+    
 }
