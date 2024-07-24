a1c63e465d46559e09ed98a17bc56314c5d7f27d
Sofiya <sofik_shu2@mail.ru>
2024-07-25 01:23:20 +0300

    add fields to crud controller with filtration

--- src/Controller/Admin/LitterCrudController.php
+++ src/Controller/Admin/LitterCrudController.php
@@ -3,26 +3,54 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Litter;
+use App\Repository\CatRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class LitterCrudController extends AbstractCrudController
 {
+
+    private CatRepository $catRepository;
+
+    public function __construct(CatRepository $catRepository)
+    {
+        $this->catRepository = $catRepository;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Litter::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('breed'),
+            TextField::new('name'),
+            DateField::new('date'),
+            AssociationField::new('catMother')
+                ->setFormTypeOptions([
+                    'query_builder' => function (CatRepository $catRepository) {
+                        return $catRepository->createQueryBuilder('c')
+                            ->where('c.gender = :gender')
+                            ->setParameter('gender', 2)
+                            ->orderBy('c.name', 'ASC');
+                    },
+                ]),
+            AssociationField::new('catFather')
+                ->setFormTypeOptions([
+                    'query_builder' => function (CatRepository $catRepository) {
+                        return $catRepository->createQueryBuilder('c')
+                            ->where('c.gender = :gender')
+                            ->setParameter('gender', 1)
+                            ->orderBy('c.name', 'ASC');
+                    },
+                ]),
         ];
     }
-    */
+    
 }
