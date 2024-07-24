4516ce0fce67f0cb34723751e6062fe93dec7845
Sofiya <sofik_shu2@mail.ru>
2024-07-24 23:58:40 +0300

    add filtration by gender

--- src/Repository/CatRepository.php
+++ src/Repository/CatRepository.php
@@ -16,20 +16,19 @@ class CatRepository extends ServiceEntityRepository
         parent::__construct($registry, Cat::class);
     }
 
-//    /**
-//     * @return Cat[] Returns an array of Cat objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   /**
+    * @return Cat[] 
+    */
+   public function findByGender(string $gender): array
+   {
+       return $this->createQueryBuilder('c')
+           ->andWhere('c.gender = :gender')
+           ->setParameter('gender', $gender)
+           ->orderBy('c.id', 'ASC')
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Cat
 //    {
