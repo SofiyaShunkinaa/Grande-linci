edb5db4cac283c1890209f55347df4ecbdb60ebd
Sofiya <sofik_shu2@mail.ru>
2024-07-24 23:35:33 +0300

    change field type and relation

--- src/Entity/Kitten.php
+++ src/Entity/Kitten.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\KittenRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Enum\StatusType;
 
 #[ORM\Entity(repositoryClass: KittenRepository::class)]
 class Kitten
@@ -20,8 +21,9 @@ class Kitten
     #[ORM\JoinColumn(nullable: false)]
     private ?Breed $breed = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $KittenStatus = null;
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?KittenStatus $kittenStatus = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
@@ -29,7 +31,7 @@ class Kitten
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Color $сolor = null;
+    private ?Color $color = null;
 
     public function getId(): ?int
     {
@@ -60,14 +62,14 @@ class Kitten
         return $this;
     }
 
-    public function getKittenStatus(): ?string
+    public function getKittenStatus(): ?KittenStatus
     {
-        return $this->KittenStatus;
+        return $this->kittenStatus;
     }
 
-    public function setKittenStatus(string $KittenStatus): static
+    public function setKittenStatus(?KittenStatus $kittenStatus): static
     {
-        $this->KittenStatus = $KittenStatus;
+        $this->kittenStatus = $kittenStatus;
 
         return $this;
     }
