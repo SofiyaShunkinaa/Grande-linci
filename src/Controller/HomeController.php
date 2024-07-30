b140a2826801b49d8cfde55e5177d34200244afc
Sofiya <sofik_shu2@mail.ru>
2024-07-31 02:02:38 +0300

    change home controller to only displaying a template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,11 +21,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(LitterRepository $litterRepository): Response
     {
-        $litter = $litterRepository->find();
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'litter'=>$litter,
         ]);
     }
 }
