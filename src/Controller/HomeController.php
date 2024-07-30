425c71f2dc776d85d3c9b7b5e5972eb5c631d0fa
Sofiya <sofik_shu2@mail.ru>
2024-07-30 23:59:46 +0300

    change home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Litter;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\LitterRepository;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(LitterRepository $litterRepository): Response
+    {
+        $litter = $litterRepository->find();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'litter'=>$litter,
         ]);
     }
 }
