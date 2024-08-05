5b4416014617b58eebb819780cdbc00c6711c8e3
Sofiya <sofik_shu2@mail.ru>
2024-08-05 19:05:00 +0300

    change home controller to return vars

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Litter;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\LitterRepository;
+use App\Repository\KittenRepository;
+use App\Repository\CatRepository;
 
 class HomeController extends AbstractController
 {
@@ -19,10 +21,18 @@ class HomeController extends AbstractController
     }
 
     #[Route('/', name: 'app_home')]
-    public function index(LitterRepository $litterRepository): Response
+    public function index(LitterRepository $litterRepository, KittenRepository $kittenRepository, CatRepository $catRepository): Response
     {
+        $litter = $litterRepository->findOneByIsActive();
+        $kittens = $kittenRepository->find4ByLitter($litter->getId());
+        $mom = $catRepository->findOneBy(['id'=>$litter->getCatMother()]);
+        $dad = $catRepository->findOneBy(['id'=>$litter->getCatFather()]);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'litter' => $litter,
+            'kittens' => $kittens,
+            'mother' => $mom,
+            'father' => $dad,
         ]);
     }
 }
