ae079d4685fda5f7cc55aee51bd2e39f340f643b
Sofiya <sofik_shu2@mail.ru>
2024-08-13 19:44:07 +0300

    try to fix request service

--- src/Service/GuestRequestService.php
+++ src/Service/GuestRequestService.php
@@ -5,34 +5,38 @@ namespace App\Service;
 use App\Entity\GuestRequest;
 use App\Form\RequestType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class GuestRequestService
 {
     private EntityManagerInterface $entityManager;
-    private Request $request;
+    private FormFactoryInterface $formFactory;
+    private SessionInterface $session;
 
-    public function __construct(EntityManagerInterface $entityManager, Request $request)
+    public function __construct(EntityManagerInterface $entityManager, FormFactoryInterface $formFactory, SessionInterface $session)
     {
         $this->entityManager = $entityManager;
-        $this->request = $request;
+        $this->formFactory = $formFactory;
+        $this->session = $session;
     }
 
-    public function createAndHandleForm(): array
+    public function createAndHandleForm(Request $request): array
     {
         $guestRequest = new GuestRequest();
-        $form = $this->createForm(RequestType::class, $guestRequest);
-        $form->handleRequest($this->$request);
-        
-        if($form->isSubmitted() && $form->isValid()){
+        $form = $this->formFactory->create(RequestType::class, $guestRequest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             $guestRequest->setRequestDate(new \DateTime());
-            
+
             $this->entityManager->persist($guestRequest);
             $this->entityManager->flush();
 
-            $this->addFlash('success', 'Your request has been submitted successfully.');
+            $this->session->getFlashBag()->add('success', 'Your request has been submitted successfully.');
         }
 
-        return ['form' => $form->createView()];
+        return ['form' => $form->createView(), 'guestRequest' => $guestRequest];
     }
 }
