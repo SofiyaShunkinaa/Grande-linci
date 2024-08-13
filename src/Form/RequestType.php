c1af4709fb465912ca88c954046d8bdc6e0f67ae
Sofiya <sofik_shu2@mail.ru>
2024-08-13 19:43:26 +0300

    add placeholders

--- src/Form/RequestType.php
+++ src/Form/RequestType.php
@@ -6,14 +6,20 @@ use App\Entity\GuestRequest;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class RequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
+        ->add('name', TextType::class, [
+            'attr' => ['placeholder' => 'Your Name']
+        ])
+        ->add('email', EmailType::class, [
+            'attr' => ['placeholder' => 'example@gmail.com']
+        ])
             ->add('phone')
             ->add('message')
         ;
