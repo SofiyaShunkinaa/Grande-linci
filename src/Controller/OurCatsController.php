61bed2c79998d2bb2b57987b4f189d763bef85f7
Sofiya <sofik_shu2@mail.ru>
2024-08-15 22:24:36 +0300

    create our cats controller

---
+++ src/Controller/OurCatsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class OurCatsController extends AbstractController
+{
+    #[Route('/our/cats', name: 'app_our_cats')]
+    public function index(): Response
+    {
+        return $this->render('our_cats/index.html.twig', [
+            'controller_name' => 'OurCatsController',
+        ]);
+    }
+}
