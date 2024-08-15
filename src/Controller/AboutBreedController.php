735d68a8b1d02f9b29f84466f8ace43365319a67
Sofiya <sofik_shu2@mail.ru>
2024-08-15 22:23:46 +0300

    create about breed controller

---
+++ src/Controller/AboutBreedController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AboutBreedController extends AbstractController
+{
+    #[Route('/about/breed', name: 'app_about_breed')]
+    public function index(): Response
+    {
+        return $this->render('about_breed/index.html.twig', [
+            'controller_name' => 'AboutBreedController',
+        ]);
+    }
+}
