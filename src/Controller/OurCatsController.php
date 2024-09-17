8bb2ef54f05d1307b07c97a3286e3281a5a472e3
Sofiya <sofik_shu2@mail.ru>
2024-09-17 10:39:38 +0300

    add another confirmation

--- src/Controller/OurCatsController.php
+++ src/Controller/OurCatsController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\CatService;
 
 class OurCatsController extends AbstractController
 {
     #[Route('/our-cats', name: 'app_our_cats')]
-    public function index(): Response
+    public function index(CatService $catService): Response
     {
+        $females = $catService->getFemaleCats();
+        $males = $catService->getMaleCats();
+        $catsCount = count($females)+count($males);
+           
+
         return $this->render('our_cats/index.html.twig', [
             'controller_name' => 'OurCatsController',
+            'females' => $females,
+            'males' => $males,
+            'count' => $catsCount,
         ]);
     }
 }
