04e6f110f04e180da0c3bbf5ce484376ee5eea97
Sofiya <sofik_shu2@mail.ru>
2024-09-23 10:16:27 +0300

    added condition to return cats array

--- src/Controller/OurCatsController.php
+++ src/Controller/OurCatsController.php
@@ -14,13 +14,56 @@ class OurCatsController extends AbstractController
     {
         $females = $catService->getFemaleCats();
         $males = $catService->getMaleCats();
-        $catsCount = count($females)+count($males);
-           
-
+        $cats = [];
+        $femaleIndex = 0;
+        $maleIndex = 0;
+       
+        while ($femaleIndex < count($females) || $maleIndex < count($males)) {
+      
+            $female = $femaleIndex < count($females) ? $females[$femaleIndex] : null;
+            $male = $maleIndex < count($males) ? $males[$maleIndex] : null;
+        
+            if ($female !== null && $male !== null) {
+                $cats[] = [
+                    'female' => $female,
+                    'male' => null,
+                ];
+                $cats[] = [
+                    'female' => null,
+                    'male' => $male,
+                ];
+                $femaleIndex++;
+                $maleIndex++;
+            } elseif ($female !== null) {
+                $cats[] = [
+                    'female' => $female,
+                    'male' => null,
+                ];
+                $femaleIndex++;
+        
+                if ($femaleIndex < count($females)) {
+                    $cats[] = [
+                        'female' => $females[$femaleIndex],
+                        'male' => null,
+                    ];
+                    $femaleIndex++;
+                }
+            } elseif ($male !== null) {
+          
+                $cats[] = [
+                    'female' => null,
+                    'male' => $male,
+                ];
+                $maleIndex++;
+            }
+        }
+        
+        $catsCount = count($cats);
+        
+     
         return $this->render('our_cats/index.html.twig', [
             'controller_name' => 'OurCatsController',
-            'females' => $females,
-            'males' => $males,
+            'cats' => $cats,
             'count' => $catsCount,
         ]);
     }
