52cd9e89ca65015a40eb37850fa0ca024060fbc8
Sofiya <sofik_shu2@mail.ru>
2024-09-23 19:09:50 +0300

    add base methods to controller

--- src/Controller/AvailableKittensController.php
+++ src/Controller/AvailableKittensController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\LitterService;
+
 
 class AvailableKittensController extends AbstractController
 {
     #[Route('/available-kittens', name: 'app_available_kittens')]
-    public function index(): Response
+    public function index(LitterService $litterService): Response
     {
+        $litters = $litterService->getAllLitters();
+        $buttons = [];
+        foreach($litters as $litter){
+            $status = $litterService->getLitterStatus($litter);
+            
+            $buttons[] = [
+                'name' => $litter->getName(),
+                'id' => $litter->getId(),
+                'status' => $status,
+            ];
+        }
+
         return $this->render('available_kittens/index.html.twig', [
             'controller_name' => 'AvailableKittensController',
+            'buttons' => $buttons,
         ]);
     }
 }
