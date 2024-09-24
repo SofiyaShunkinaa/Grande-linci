9b76fe153150bb793d535fbbad07300e535ef5b0
Sofiya <sofik_shu2@mail.ru>
2024-09-24 23:59:33 +0300

    add methods

--- src/Controller/AvailableKittensController.php
+++ src/Controller/AvailableKittensController.php
@@ -4,14 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\LitterService;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class AvailableKittensController extends AbstractController
 {
-    #[Route('/available-kittens', name: 'app_available_kittens')]
-    public function index(LitterService $litterService): Response
+    #[Route('/available-kittens/{id}', name: 'app_available_kittens', methods: ['GET'])]
+    public function index($id, LitterService $litterService, Request $request): Response
     {
         $litters = $litterService->getAllLitters();
         $buttons = [];
@@ -24,10 +25,47 @@ class AvailableKittensController extends AbstractController
                 'status' => $status,
             ];
         }
+        if($id && $id !== "default"){
+            [$litter, $mom, $dad] = $litterService->getLitterById($id); 
+            $kittens = $litterService->getAllKittens($litter);
+        }
+        else{
+            [$litter, $mom, $dad] = $litterService->getLitter(); 
+            $kittens = $litterService->getAllKittens($litter);
+        }
+       
+        if($request->isXmlHttpRequest()){
+            return new JsonResponse([
+                'buttons' => $buttons,
+                'litter' => [
+                    'name' => $litter->getName(),
+                    'date' => $litter->getDate()->format('Y-m-d'),
+                ],
+                'mother' => [
+                    'name' => $mom->getName(),
+                    'imageLink' => $mom->getImageLink(),
+                ],
+                'father' => [
+                    'name' => $dad->getName(),
+                    'imageLink' => $dad->getImageLink(),
+                ],
+                'kittens' => array_map(function($kitten) {
+                    return [
+                        'name' => $kitten->getName(),
+                        'imageLink' => $kitten->getImageLink(),
+                        'kittenStatus' => $kitten->getKittenStatus(),
+                    ];
+                }, $kittens),
+            ]);
+        }
 
         return $this->render('available_kittens/index.html.twig', [
             'controller_name' => 'AvailableKittensController',
             'buttons' => $buttons,
+            'litter' => $litter,
+            'mother' => $mom,
+            'father' => $dad,
+            'kittens' => $kittens,
         ]);
     }
 }
