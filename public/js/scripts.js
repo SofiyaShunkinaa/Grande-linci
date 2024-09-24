cf10dcd71e6fdc77e74d8f54cbc71e0160f0d68e
Sofiya <sofik_shu2@mail.ru>
2024-09-24 23:59:16 +0300

    add ajax function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -29,4 +29,42 @@ const swiperKittens = new Swiper('.swiper-kittens', {
 const input = document.querySelector("#request_phone");
 window.intlTelInput(input, {
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.8.0/build/js/utils.js",
-});    
+});   
+
+function selectLitter(litterId){
+    fetch(`/available-kittens/${litterId}`, {
+        method: 'GET',
+        headers: {
+            'X-Reauested-With': 'XMLHttpRequest'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('litter-block').innerHTML = `
+                <div class="title">
+                    <h2>LITTER ${data.litter.name}</h2>
+                    <p>${new Date(data.litter.date).toLocaleDateString()}</p>
+                </div>
+                <div class="section-inner">
+                    <div class="cat-parent">
+                        <img src="/img/cats/${data.mother.imageLink}" alt="Mother Cat">
+                        <h4>${data.mother.name}</h4>
+                    </div>
+                    <div class="cat-parent">
+                        <img src="/img/cats/${data.father.imageLink}" alt="Father Cat">
+                        <h4>${data.father.name}</h4>
+                    </div>
+                </div>
+                <div class="kittens">
+                    ${data.kittens.map(kitten => `
+                        <div class="kitten">
+                            <img src="/img/kittens/${kitten.imageLink}" alt="${kitten.name}">
+                            <h4>${kitten.name}</h4>
+                            <span class="status ${kitten.kittenStatus.toLowerCase()}">${kitten.kittenStatus}</span>
+                        </div>
+                    `).join('')}
+                </div>
+            `;
+        })
+    .catch(error => console.error('Error: ', error));    
+}
